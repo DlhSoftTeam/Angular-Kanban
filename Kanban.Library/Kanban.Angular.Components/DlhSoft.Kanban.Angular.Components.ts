40f1be21ba2ed661612981be56c84a79f56c879e
DlhSoft Team <Contact@DlhSoft.com>
2015-12-09 19:20:50 +0200

    BindToController, Angular module refactoring

--- Kanban.Library/Kanban.Angular.Components/DlhSoft.Kanban.Angular.Components.ts
+++ Kanban.Library/Kanban.Angular.Components/DlhSoft.Kanban.Angular.Components.ts
@@ -1,34 +1,39 @@
-﻿declare var angular;
+﻿module DlhSoft.Controls {
+    export module KanbanBoard {
+        export var defaultStates = [
+            { content: 'New' },
+            { content: 'Active' },
+            { content: 'Resolved' },
+            { content: 'Closed' }
+        ];
+        export function getItemsInGroupAndState(group, state) {
+            var itemsInGroupAndState = [];
+            for (var i = 0; i < this.items.length; i++) {
+                var item = this.items[i];
+                if (item.group == group && item.state == state)
+                    itemsInGroupAndState.push(item);
+            }
+            return itemsInGroupAndState;
+        }
+    }
+}
 
+declare var angular;
 angular.module('DlhSoft.Kanban.Angular.Components', [])
     .directive('dsKanbanBoard', function () {
         return {
             restrict: 'EAC',
             replace: true,
             transclude: true,
-            scope: {
+            bindToController: {
                 items: '=',
                 groups: '=',
                 states: '='
             },
-            controller: function ($scope) {
-                var items = this.items = $scope.items;
-                this.groups = $scope.groups;
-                this.states = $scope.states ? $scope.states : [
-                    { content: 'New' },
-                    { content: 'Active' },
-                    { content: 'Resolved' },
-                    { content: 'Closed' }
-                ];
-                this.getItemsInGroupAndState = function (group, state) {
-                    var itemsInGroupAndState = [];
-                    for (var i = 0; i < items.length; i++) {
-                        var item = items[i];
-                        if (item.group == group && item.state == state)
-                            itemsInGroupAndState.push(item);
-                    }
-                    return itemsInGroupAndState;
-                }
+            controller: function () {
+                if (!this.states)
+                    this.states = DlhSoft.Controls.KanbanBoard.defaultStates;
+                this.getItemsInGroupAndState = DlhSoft.Controls.KanbanBoard.getItemsInGroupAndState;
             },
             controllerAs: 'dskb',
             templateUrl: 'DlhSoft.Kanban.Angular.Components/kanban-board.html'
--- Kanban.Library/Kanban.Angular.Components/app.ts
+++ Kanban.Library/Kanban.Angular.Components/app.ts
@@ -1,23 +1,23 @@
-﻿declare var angular;
+﻿/// <reference path='./DlhSoft.Kanban.Angular.Components.ts'/>
 
-/// <reference path='./DlhSoft.Kanban.Angular.Components.ts'/>
+var state1 = { content: 'New' }, state2 = { content: 'In progress' }, state3 = { content: 'Done' };
+var states = [state1, state2, state3];
+var group1 = { content: 'Story 1' }, group2 = { content: 'Story 2' };
+var groups = [group1, group2];
+var items = [
+    { content: 'Task 1', group: group1, state: state1 },
+    { content: 'Task 2', group: group1, state: state2 },
+    { content: 'Task 3', group: group1, state: state1 },
+    { content: 'Task 4', group: group2, state: state1 },
+    { content: 'Task 5', group: group2, state: state2 },
+    { content: 'Task 6', group: group2, state: state2 },
+    { content: 'Task 7', group: group2, state: state3 }
+];
 
+declare var angular;
 angular.module("KanbanBoardSample", ["DlhSoft.Kanban.Angular.Components"])
     .controller("MainController", ["$scope", function ($scope) {
-        var state1 = { content: 'New' }, state2 = { content: 'In progress' }, state3 = { content: 'Done' };
-        $scope.states = [state1, state2, state3];
-        var group1 = { content: 'Story 1' }, group2 = { content: 'Story 2' };
-        $scope.groups = [group1, group2];
-        $scope.items = [
-            { content: 'Task 1', group: group1, state: state1 },
-            { content: 'Task 2', group: group1, state: state2 },
-            { content: 'Task 3', group: group1, state: state1 },
-            { content: 'Task 4', group: group2, state: state1 },
-            { content: 'Task 5', group: group2, state: state2 },
-            { content: 'Task 6', group: group2, state: state2 },
-            { content: 'Task 7', group: group2, state: state3 }
-        ];
+        $scope.states = states;
+        $scope.groups = groups;
+        $scope.items = items;
     }]);
-
-window.onload = () => {
-};
